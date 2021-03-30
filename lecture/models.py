046a4ee791aa02217ed113f333ec5d83b648a76f
damin0320 <damin0320@gmail.com>
2021-03-30 19:31:38 +0900

    SETTING : models.py set

---
+++ lecture/models.py
@@ -0,0 +1,106 @@
+from django.db import models
+
+from user.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    
+    class Meta:
+        db_table = 'categories'
+        
+
+class SubCategory(models.Model):
+    name     = models.CharField(max_length=20)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'subcategories'
+
+
+class Difficulty(models.Model):
+    level = models.CharField(max_length=10)
+    
+    class Meta:
+        db_table = 'difficulties'
+        
+
+class Hashtag(models.Model):
+    tag              = models.CharField(max_length=20, unique=True)
+    lectures         = models.ManyToManyField('Lecture', through='LectureHashtag')
+    pending_lectures = models.ManyToManyField('PendingLecture', through='PendingLectureHashtag')
+    
+    class Meta:
+        db_table = 'hashtags'
+        
+
+class Lecture(models.Model):
+    title         = models.CharField(max_length=50)
+    price         = models.DecimalField(max_digits=12, decimal_places=2)
+    discount_rate = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    description   = models.TextField(default='HTML')
+    down_payment  = models.DecimalField(max_digits=2, decimal_places=0, default=5)
+    created_at    = models.DateTimeField(auto_now_add=True)
+    updated_at    = models.DateTimeField(auto_now=True)
+    user          = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category      = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    sub_category  = models.ForeignKey('SubCategory', on_delete=models.SET_NULL, null=True)
+    difficulty    = models.ForeignKey('Difficulty', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        db_table        = 'lectures'
+        unique_together = ('user', 'title')
+        
+
+class LectureHashtag(models.Model):
+    lecture = models.ForeignKey('Lecture', on_delete=models.SET_NULL, null=True)
+    hashtag = models.ForeignKey('Hashtag', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        db_table = 'lectures_hashtags'
+        unique_together = ('lecture', 'hashtag')
+        
+
+class PendingLecture(models.Model):
+    title             = models.CharField(max_length=45)
+    cover_image_url   = models.CharField(max_length=2000, default='cover_image')
+    thumbnail_url     = models.CharField(max_length=2000, default='thumbnail')
+    summary_image_url = models.CharField(max_length=2000, default='summary_image')
+    is_open           = models.BooleanField(default=False)
+    created_at        = models.DateTimeField(auto_now_add=True)
+    updated_at        = models.DateTimeField(auto_now=True)
+    user              = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category          = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    sub_category      = models.ForeignKey('SubCategory', on_delete=models.SET_NULL, null=True)
+    difficulty        = models.ForeignKey('Difficulty', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        db_table        = 'pending_lectures'
+        unique_together = ('user', 'title')
+        
+
+class Introduction(models.Model):
+    detail          = models.CharField(max_length=300, null=True)
+    image_url       = models.CharField(max_length=2000)
+    pending_lecture = models.ForeignKey('PendingLecture', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        db_table        = 'introductions'
+        unique_together = ('detail', 'image_url')
+        
+
+class PendingLectureHashtag(models.Model):
+    pending_lecture = models.ForeignKey('PendingLecture', on_delete=models.SET_NULL, null=True)
+    hashtag         = models.ForeignKey('Hashtag', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        db_table        = 'pending_lectures_hashtags'
+        unique_together = ('pending_lecture', 'hashtag')
+
+
+class Vote(models.Model):
+    user            = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    pending_lecture = models.ForeignKey('PendingLecture', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        db_table = 'votes'
---
+++ user/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class User(models.Model):
+    username     = models.CharField(max_length=20)
+    email        = models.EmailField(max_length=45, unique=True)
+    password     = models.CharField(max_length=300)
+    is_creator   = models.BooleanField(default=False)
+    voting_power = models.IntegerField(default=20)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'users'
+
+
+class Creator(models.Model):
+    nickname          = models.CharField(max_length = 20, unique=True)
+    phonenumber       = models.CharField(max_length = 13, unique=True)
+    introduction      = models.CharField(max_length = 200)
+    profile_image_url = models.CharField(max_length=2000)
+    user              = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        db_table = 'creators'
