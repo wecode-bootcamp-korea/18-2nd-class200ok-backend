cb9776030fc3b65442063c097def8fa5842c652f
damin0320 <damin0320@gmail.com>
2021-04-02 17:19:12 +0900

    FIX : user/models.py user and creator(+kakao_id, +image_url)

--- user/models.py
+++ user/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class User(models.Model):
     username     = models.CharField(max_length=20)
     email        = models.EmailField(max_length=45, unique=True)
-    password     = models.CharField(max_length=300)
+    password     = models.CharField(max_length=300, null=True)
+    kakao_id     = models.CharField(max_length=20)
     is_creator   = models.BooleanField(default=False)
     voting_power = models.IntegerField(default=20)
     created_at   = models.DateTimeField(auto_now_add=True)
@@ -19,6 +20,7 @@ class Creator(models.Model):
     phonenumber       = models.CharField(max_length=13, unique=True)
     introduction      = models.CharField(max_length=200)
     profile_image_url = models.CharField(max_length=2000)
+    image_url         = models.CharField(max_length=2000, default='image')
     user              = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
     
     class Meta:
