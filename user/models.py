932b83524d08922a1e2373d2d32727aa16dbc4ba
damin0320 <damin0320@gmail.com>
2021-04-04 17:41:19 +0900

    MODIFIED : edited models.py

--- user/models.py
+++ user/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class User(models.Model):
     username     = models.CharField(max_length=20)
-    email        = models.EmailField(max_length=45, unique=True)
+    email        = models.EmailField(max_length=45, null=True, unique=True)
     password     = models.CharField(max_length=200, null=True)
-    kakao_id     = models.CharField(max_length=20)
+    kakao_id     = models.CharField(max_length=20, unique=True)
     is_creator   = models.BooleanField(default=False)
     voting_power = models.IntegerField(default=20)
     created_at   = models.DateTimeField(auto_now_add=True)
@@ -13,7 +13,7 @@ class User(models.Model):
     
     class Meta:
         db_table = 'users'
-
+        
 
 class Creator(models.Model):
     nickname          = models.CharField(max_length=20, unique=True)
