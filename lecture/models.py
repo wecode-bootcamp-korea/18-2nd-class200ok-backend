c3a8507af0343fcb185fddb7d7c6462ad533c7d9
Wang Lee <wang>
2021-04-06 16:28:28 +0900

    Edit: Charfield size reduction

--- lecture/models.py
+++ lecture/models.py
@@ -78,8 +78,8 @@ class PendingLectureHashtag(models.Model):
         unique_together = ('pending_lecture', 'hashtag')
 
 class Introduction(models.Model):
-    detail          = models.CharField(max_length=300, null=True)
-    image_url       = models.URLField()
+    detail          = models.CharField(max_length=250, null=True)
+    image_url       = models.CharField(max_length=250)
     pending_lecture = models.ForeignKey('PendingLecture', on_delete=models.SET_NULL, null=True)
     
     class Meta:
