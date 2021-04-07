b6d568289b0f7a7452605fb8ea649bdbaf1d7c24
Wang Lee <wang>
2021-04-07 21:19:39 +0900

    creator views & decorator added & possible exceptions added

--- creator/views.py
+++ creator/views.py
@@ -1,23 +1,18 @@
 import json, bcrypt, jwt, boto3, os, logging, uuid
 from json import JSONDecodeError
 
-
 from django.views import View
 from django.http  import JsonResponse
-from django.core.exceptions import ObjectDoesNotExist
-
+from django.db    import transaction, IntegrityError
 
 from my_settings            import AWS_STORAGE_BUCKET_NAME
 from creator.custom_storage import MediaStorage
-
-
-from lecture.models import (
+from lecture.models         import (
                             Category, SubCategory, Difficulty,
                             Hashtag, PendingLecture, PendingLectureHashtag,
                             Introduction, Vote)
-from user.models    import User, Creator
-
-from utils.decorators import auth_check
+from user.models            import User, Creator
+from utils.decorators       import auth_check
 
 
 class BasicInformationView(View):
@@ -51,3 +46,122 @@ class BasicInformationView(View):
             return JsonResponse({'message': 'TYPE_ERROR'}, status=400)
         except ObjectDoesNotExist:
             return JsonResponse({'message' : 'USER_DOES_NOT_EXIST'}, status=400)
+
+class TitleAndCoverView(View):
+    @auth_check
+    def post(self, request, *args, **kwargs):
+        try:
+            user = request.user
+            file_obj = request.FILES.get('cover_image')
+            title = request.POST.get('title')
+
+            file_name = file_obj.name
+            file_path_within_bucket = str(uuid.uuid4()) + file_name
+
+            media_storage = MediaStorage()
+            media_storage.save(file_path_within_bucket, file_obj)
+            file_url = media_storage.url(file_path_within_bucket)
+            PendingLecture.objects.filter(user=user).update(cover_image_url=file_url, title=title)
+            
+            return JsonResponse({'result' : 'Title and cover image uploaded'}, status=201)
+        
+        except AttributeError:
+            return JsonResponse({"message": "Attribute_Error, possibly image not received"}, status=400)
+        except PendingLecture.DoesNotExist:
+            return JsonResponse({"message": "Please submit basic information from https://class101.net/creators/compact/edit/hZr6aB03qCE6JCGuXIjv/basic-information"}, status=403)
+
+
+class ClassIntroductionView(View):
+    @auth_check
+    def post(self, request):
+        try: 
+            user = request.user
+            pending_lecture = PendingLecture.objects.get(user=user)
+            file_objs = request.FILES.getlist('introduction_image')
+            details = request.POST.getlist('detail')
+
+            for i in range(len(file_objs)):
+                file_name = file_objs[i].name
+                file_path_within_bucket = str(uuid.uuid4()) + file_name
+
+                media_storage = MediaStorage()
+                media_storage.save(file_path_within_bucket, file_objs[i])
+                file_url = media_storage.url(file_path_within_bucket)
+                Introduction.objects.create(detail=details[i], image_url=file_url, pending_lecture=pending_lecture)
+                print(file_name)
+            return JsonResponse({'result' : 'Introduction image(s) and detail(s) uploaded'}, status=201)
+        
+        except AttributeError:
+            return JsonResponse({"message": "Attribute_Error, possibly image not received"}, status=400)
+        except PendingLecture.DoesNotExist:
+            return JsonResponse({"message": "Please submit basic information from https://class101.net/creators/compact/edit/hZr6aB03qCE6JCGuXIjv/basic-information"}, status=403)
+
+
+class SummaryView(View):
+    @auth_check
+    def post(self, request):
+        try:
+            user = request.user
+            pending_lecture = PendingLecture.objects.get(user=user)
+            file_obj = request.FILES.get('summary_image')
+            tags = request.POST.getlist('tag')
+
+            file_name = file_obj.name
+            file_path_within_bucket = str(uuid.uuid4()) + file_name
+
+            media_storage = MediaStorage()
+            media_storage.save(file_path_within_bucket, file_obj)
+            file_url = media_storage.url(file_path_within_bucket)
+            PendingLecture.objects.filter(user=user).update(summary_image_url=file_url)
+
+            for tag in tags:
+                if Hashtag.objects.filter(tag=tag).exists():
+                    hashtag=Hashtag.objects.get(tag=tag)
+                    if PendingLectureHashtag.objects.filter(pending_lecture=pending_lecture, hashtag=hashtag).exists():
+                        pass
+                    else:
+                        PendingLectureHashtag.objects.create(pending_lecture=pending_lecture, hashtag=hashtag)
+                else:
+                    hashtag=Hashtag.objects.create(tag=tag)
+                    PendingLectureHashtag.objects.create(pending_lecture=pending_lecture, hashtag=hashtag)
+
+            return JsonResponse({'result' : 'Summary and tag(s) uploaded'}, status=201)
+        
+        except AttributeError:
+            return JsonResponse({"message": "Attribute_Error, possibly image not received"}, status=400)
+        except PendingLecture.DoesNotExist:
+            return JsonResponse({"message": "Please submit basic information from https://class101.net/creators/compact/edit/hZr6aB03qCE6JCGuXIjv/basic-information"}, status=403)
+
+
+class CreatorIntroductionView(View):
+    @auth_check
+    def post(self, request):
+        try:            
+            user = request.user
+            pending_lecture = PendingLecture.objects.get(user=user)
+            file_obj = request.FILES.get('profile_image')
+            data = request.POST
+            nickname = data.get('nickname')
+            phonenumber = data.get('phonenumber')
+            introduction = data.get('introduction')
+
+            file_name = file_obj.name
+            file_path_within_bucket = str(uuid.uuid4()) + file_name
+
+            media_storage = MediaStorage()
+            media_storage.save(file_path_within_bucket, file_obj)
+            file_url = media_storage.url(file_path_within_bucket)
+
+            if Creator.objects.filter(user=user).exists():
+                Creator.objects.filter(user=user).update(nickname=nickname, phonenumber=phonenumber, introduction=introduction, profile_image_url=file_url)
+                return JsonResponse({'result' : 'Creator profile updated'}, status=201)
+            else:
+                Creator.objects.create(nickname=nickname, phonenumber=phonenumber, introduction=introduction, profile_image_url=file_url)
+                return JsonResponse({'result' : 'Creator profile uploaded'}, status=201)
+
+        except AttributeError:
+            return JsonResponse({"message": "Attribute_Error, possibly image not received"}, status=400)
+        except IntegrityError:
+            return JsonResponse({"message": "IntegrityError, possibly duplicated entry for key 'nickname' or 'phonenumber'"}, status=400)
+        except PendingLecture.DoesNotExist:
+            return JsonResponse({"message": "Please submit basic information from https://class101.net/creators/compact/edit/hZr6aB03qCE6JCGuXIjv/basic-information"}, status=403)
