e49ec617abd0aefd6552d64da3687af7d881b513
Wang Lee <wang>
2021-04-07 21:19:39 +0900

    creator views & decorator added & possible exceptions added

--- creator/urls.py
+++ creator/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
-from creator.views import BasicInformationView
-# from creator.views import BasicInformationView, TitleAndCoverView, ClassIntroduction, SummaryView, CreatorIntroductionView, FinalChecklistsView
+from creator.views import TitleAndCoverView, ClassIntroductionView, SummaryView, CreatorIntroductionView, BasicInformationView
 
 urlpatterns = [
     path('/basic-information', BasicInformationView.as_view()),
-    # path('/title-and-cover', TitleAndCoverView.as_view()),
-    # path('/class-introduction', ClassIntroduction.as_view()),
-    # path('/summary', SummaryView.as_view()),
-    # path('/creator-introduction', CreatorIntroductionView.as_view()),
-    # path('/final-checklists', FinalChecklistsView.as_view()),
+    path('/title-and-cover', TitleAndCoverView.as_view()),
+    path('/class-introduction', ClassIntroductionView.as_view()),
+    path('/summary', SummaryView.as_view()),
+    path('/creator-introduction', CreatorIntroductionView.as_view()),
 ]
